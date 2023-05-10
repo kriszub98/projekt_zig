295adcc90adf3bb3495b789ddc20955bfe3e1585
Krzysztof Zubrzycki <kriszub98@gmail.com>
2023-05-10 21:20:23 +0200

    Filtering empty elements, now saves filtered data with content

--- filter_medyczka.py
+++ filter_medyczka.py
@@ -6,6 +6,7 @@ import regex
 
 def filter_empty(unfiltered_list):
     filtered_list = [d for d in unfiltered_list if "content" in d]
+    filtered_list = [d for d in filtered_list if not d['content'] == ""]
     return filtered_list
 
 
@@ -34,10 +35,11 @@ def filter_words(unfiltered_list):
                         "WYSYŁKA", "Wysyłka", "wysyłka",
                         "WYSYLKA", "Wysylka", "wysylka", "Where", "How to",
                         "everprof", "chat", "cam", "porn", "sex", "buy", "Buy", "BUY", "website", "video",
-                        "tits", "ass"]
+                        "tits", "ass", "pussy"]
     filtered_list = [d for d in unfiltered_list if not any(word in d["content"] for word in words_to_exclude)]
     filtered_list = [d for d in filtered_list if not any(word in d["title"] for word in words_to_exclude)]
     return filtered_list
+
 
 def filter_author(unfiltered_list):
     authors_to_exclude = ["fahij76111", "Kegankar", "calebjames", "brila", "WitekZoobseics", "Witeklallwappy", "chptiepthi6", "huongviet3933", "Robertbot"]
@@ -47,9 +49,9 @@ def filter_author(unfiltered_list):
 
 def save_to_csv(filtered_list, file):
     writer = csv.writer(file)
-    writer.writerow(["source", "date", "title"])
+    writer.writerow(["source", "date", "title", "content"])
     for d in filtered_list:
-        writer.writerow(['medyczka', d['date'], d['title']])
+        writer.writerow(['medyczka', d['date'], d['title'], d['content']])
 
 
 with open('medyczka.json', 'r', encoding="utf-8") as fr:
