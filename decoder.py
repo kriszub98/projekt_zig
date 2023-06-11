ddcfd3e07afbe48568982ec09802fa37f9e45da8
Krzysztof Zubrzycki <kriszub98@gmail.com>
2023-06-11 16:07:07 +0200

    change json data to csv, each record starts with new line

--- decoder.py
+++ decoder.py
@@ -3,17 +3,27 @@ import os
 import csv
 import json
 
-dataset = pd.read_csv("final_dataset.csv")
+dataset = pd.read_csv("final_with_keywords.csv")
 result_file = open("keytool_with_data.csv", "w", encoding="utf-8", newline="")
 writer = csv.writer(result_file)
+
+header = ['ID', 'Source', 'Title', 'Content', 'Timestamp', 'Ours', 'Voicelab', 'Clarin', 'Textclass', 'Ner']
+writer.writerow(header)
+
 
 for root, dirs, files in os.walk("Keytool"):
     for filename in files:
         with open(f"Keytool/{filename}", encoding="utf-8") as file:
             _id = filename.replace(".txt", "")
 
-            keywords = file.readline()
-            dataset.loc[dataset["ID"] == int(_id), "Keywords"] = keywords
+            keywords = json.load(file)
+            # print(keywords["voicelab"])
+            # dataset.loc[dataset["ID"] == int(_id), "Keywords"] = keywords
+            dataset.loc[dataset["ID"] == int(_id), "Voicelab"] = "\n".join(keywords['voicelab']['labels'])
+            # dataset.loc[dataset["ID"] == int(_id), "Voicelab"] = json.dumps(keywords['voicelab'])
+            dataset.loc[dataset["ID"] == int(_id), "Clarin"] = "\n".join(keywords['clarin']['labels'])
+            dataset.loc[dataset["ID"] == int(_id), "Textclass"] = "\n".join(keywords['textclass']['labels'])
+            dataset.loc[dataset["ID"] == int(_id), "Ner"] = "\n".join(keywords['ner']['labels'])
 
             search_result = dataset.loc[dataset['ID'] == int(_id)]
             writer.writerow(search_result.values[0])
